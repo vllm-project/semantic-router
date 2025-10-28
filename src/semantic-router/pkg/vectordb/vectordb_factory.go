39ef623b1aba2be7375adfa516f91f7e6076ad13
Naveen Unnikrishnan <naveenunnikrishnan98@gmail.com>
2025-10-28 12:14:42 -0400

    [vectordb] Add vectordb factory

---
+++ src/semantic-router/pkg/vectordb/vectordb_factory.go
@@ -0,0 +1,54 @@
+package vectordb
+
+import (
+	"fmt"
+
+	"github.com/vllm-project/semantic-router/src/semantic-router/pkg/observability"
+)
+
+func NewVectorDbBackend(config VectorDbConfig) (VectorDbBackend, error) {
+	if err := ValidateVectorDbConfig(config); err != nil {
+		return nil, fmt.Errorf("invalid vector DB config: %w", err)
+	}
+	switch config.Type {
+	case ChromaVectorDbType:
+		observability.Debugf("Creating chroma backend - Endpoint: %s, Collection: %s", config.Endpoint, config.Collection)
+		options := ChromaVectorDbOptions{
+			Endpoint:   config.Endpoint,
+			Collection: config.Collection,
+		}
+		vectorDb, err := NewChromaVectorDb(options)
+		if err != nil {
+			observability.Errorf("Error instantiating Chroma DB: %w", err)
+			return nil, err
+		}
+		return vectorDb, nil
+	case MilvusVectorDbType:
+		observability.Debugf("Creating milvus backend - Endpoint: %s, Collection: %s", config.Endpoint, config.Collection)
+		options := MilvusVectorDbOptions{
+			Endpoint:   config.Endpoint,
+			Collection: config.Collection,
+		}
+		vectorDb, err := NewMilvusVectorDb(options)
+		if err != nil {
+			observability.Errorf("Error instantiating Chroma DB: %w", err)
+			return nil, err
+		}
+		return vectorDb, nil
+	default:
+		return nil, fmt.Errorf("vector DB type %s is not implemented", config.Type)
+	}
+}
+
+func ValidateVectorDbConfig(config VectorDbConfig) error {
+	switch config.Type {
+	// Add backend-specific validations once implemented
+	case ChromaVectorDbType, MilvusVectorDbType:
+		if config.Endpoint == "" {
+			return fmt.Errorf("endpoint not specified")
+		}
+	default:
+		return fmt.Errorf("vector DB type not specified")
+	}
+	return nil
+}
