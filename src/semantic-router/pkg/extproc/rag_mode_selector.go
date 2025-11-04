83dab9fa56a3e47b47291d7258b3e2f0b5cacca3
Naveen Unnikrishnan <naveenunnikrishnan98@gmail.com>
2025-11-04 12:13:58 -0500

    [rag] Implement rag_mode_selector.getRAGChunks

--- src/semantic-router/pkg/extproc/rag_mode_selector.go
+++ src/semantic-router/pkg/extproc/rag_mode_selector.go
@@ -1,10 +1,12 @@
 package extproc
 
 import (
+	"fmt"
 	"strings"
-	"github.com/vllm-project/semantic-router/src/semantic-router/pkg/observability"
-)
 
+	"github.com/vllm-project/semantic-router/src/semantic-router/pkg/observability"
+	"github.com/vllm-project/semantic-router/src/semantic-router/pkg/vectordb"
+)
 
 // getRAGStrategy gets the specified RAG strategy for the categoryName
 func (r *OpenAIRouter) getRAGStrategy(categoryName string) string {
@@ -16,26 +18,26 @@ func (r *OpenAIRouter) getRAGStrategy(categoryName string) string {
 				ragStrategy = category.RagStrategy
 				if category.RagDescription != "" {
 					observability.Infof("RAG strategy: Category '%s', RAG strategy '%s', RAG description '%s'",
-					categoryName, ragStrategy, category.RagDescription)
+						categoryName, ragStrategy, category.RagDescription)
 				}
 				observability.Infof("RAG strategy: Category '%s', RAG strategy '%s'",
-				categoryName, ragStrategy)
-				} else {
-					observability.Infof("Category '%s' has no RAG strategy configured, defaulting to the default RAG strategy", categoryName)
-				}
-				break
+					categoryName, ragStrategy)
+			} else {
+				observability.Infof("Category '%s' has no RAG strategy configured, defaulting to the default RAG strategy", categoryName)
 			}
+			break
 		}
-		return ragStrategy
 	}
-	
+	return ragStrategy
+}
+
 // getRAGDecision determines if RAG should be used based on the query and category
 func (r *OpenAIRouter) getRAGDecision(query string, categoryName string) bool {
 	// Create a strategy to decision map with string keys and bool values
 
-	ragStrategy :=  r.getRAGStrategy(categoryName)
+	ragStrategy := r.getRAGStrategy(categoryName)
 	// TODO: Handle Either RagConfig or DefaultStrategy could be empty
-	ragDefault := r.Config.Rag.DefaultStrategy 
+	ragDefault := r.Config.Rag.DefaultStrategy
 
 	// If no RAG strategy is set use the default strategy
 	if ragStrategy == "" {
@@ -55,13 +57,24 @@ func (r *OpenAIRouter) getRAGDecision(query string, categoryName string) bool {
 	}
 }
 
+func (r *OpenAIRouter) getVectorDBForCategory(categoryName string) vectordb.VectorDbBackend {
+	for _, category := range r.Config.Categories {
+		if category.Name == categoryName {
+			// Use category-specific knowledge base
+			if len(category.KnowledgeBases) > 0 {
+				return r.KnowledgeBases[category.KnowledgeBases[0]]
+			}
+			break
+		}
+	}
+	return nil
+}
+
 // getRAGChunks gets the RAG chunks which are sent to addRAGChunksToRequestBody as 'ragChunks []string'
-func (r *OpenAIRouter) getRAGChunks(categoryName string, query string) []string {
-	// TODO: Naveen, Ani K
-	// use categoryName to get the relevant knowledge base
-	// send the query to the knowledge base to get the relevant chunks
-	// may have to embed the query before querying vector db
-
-	return []string{"This is a sample RAG chunk."}
-
+func (r *OpenAIRouter) getRAGChunks(categoryName string, query string) ([]string, error) {
+	vector_db := r.getVectorDBForCategory(categoryName)
+	if vector_db == nil {
+		return nil, fmt.Errorf("no vector DB backend found for category %s", categoryName)
+	}
+	return vector_db.Query(query)
 }
