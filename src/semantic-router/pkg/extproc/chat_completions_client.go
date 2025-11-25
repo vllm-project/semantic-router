87b5f81fb3cfd656f510fcdabb750deb3c2d3d72
Naveen Unnikrishnan <naveenunnikrishnan98@gmail.com>
2025-11-25 12:19:52 -0800

    Add queryModel method to query chat competions client

--- src/semantic-router/pkg/extproc/chat_completions_client.go
+++ src/semantic-router/pkg/extproc/chat_completions_client.go
@@ -1,8 +1,9 @@
 package extproc
 
 import (
-	"fmt"
 	"context"
+	"fmt"
+
 	"github.com/openai/openai-go"
 	"github.com/openai/openai-go/option"
 	"github.com/vllm-project/semantic-router/src/semantic-router/pkg/observability"
@@ -29,20 +30,39 @@ func (c *ChatCompletionClient) queryModelForLogProbs(openAIRequest *openai.ChatC
 	openAIRequest.Logprobs = openai.Bool(true)
 	openAIRequest.TopLogprobs = openai.Int(5)
 	openAIRequest.MaxCompletionTokens = openai.Int(100)
-	
+
 	ctx := context.Background()
 
 	// call the client
-    resp, err := c.client.Chat.Completions.New(ctx, *openAIRequest)
-    if err != nil {
+	resp, err := c.client.Chat.Completions.New(ctx, *openAIRequest)
+	if err != nil {
 		return nil, fmt.Errorf("error calling chat completions: %w", err)
 	}
 
 	if len(resp.Choices) == 0 {
 		return nil, fmt.Errorf("no choices returned")
 	}
-	
+
 	logprobs := resp.Choices[0].Logprobs
 	observability.Debugf("Full logprobs: %+v", logprobs)
 	return &logprobs, nil
+}
+
+func (c *ChatCompletionClient) queryModel(openAIRequest *openai.ChatCompletionNewParams, matchedModel string) (string, error) {
+	observability.Infof("Querying '%s'", matchedModel)
+	openAIRequest.Model = openai.ChatModel(matchedModel)
+
+	ctx := context.Background()
+
+	// call the client
+	resp, err := c.client.Chat.Completions.New(ctx, *openAIRequest)
+	if err != nil {
+		return "", fmt.Errorf("error calling chat completions: %w", err)
+	}
+
+	if len(resp.Choices) == 0 {
+		return "", fmt.Errorf("no choices returned")
+	}
+
+	return resp.Choices[0].Message.Content, nil
 }
