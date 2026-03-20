7eb5443003f975c3d6129d8a3d03a03fb6390e9a
yangw <wuyangmuc@gmail.com>
2026-03-20 17:04:37 +0800

    fix lint
    Signed-off-by: yangw <wuyangmuc@gmail.com>

--- src/semantic-router/pkg/extproc/res_filter_hallucination.go
+++ src/semantic-router/pkg/extproc/res_filter_hallucination.go
@@ -8,6 +8,7 @@ import (
 
 	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	ext_proc "github.com/envoyproxy/go-control-plane/envoy/service/ext_proc/v3"
+
 	"github.com/vllm-project/semantic-router/src/semantic-router/pkg/config"
 	"github.com/vllm-project/semantic-router/src/semantic-router/pkg/headers"
 	"github.com/vllm-project/semantic-router/src/semantic-router/pkg/observability/logging"
