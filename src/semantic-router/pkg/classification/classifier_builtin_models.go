6a2d71ade586a30bebccb7b3b032b54d2f33b854
Raj Bhargav <72274012+p172913@users.noreply.github.com>
2026-07-04 23:42:56 +0530

    To resolve Feature disabled / model not loaded issue
    Signed-off-by: Raj Bhargav <72274012+p172913@users.noreply.github.com>

--- src/semantic-router/pkg/classification/classifier_builtin_models.go
+++ src/semantic-router/pkg/classification/classifier_builtin_models.go
@@ -67,6 +67,7 @@ func (c *Classifier) initializeJailbreakClassifier() error {
 			"mode":      c.Config.PromptGuard.Protocol,
 			"model_ref": externalCfg.ModelName,
 		})
+		c.jailbreakModelReady = true
 		return nil
 	}
 
@@ -86,7 +87,11 @@ func (c *Classifier) initializeJailbreakClassifier() error {
 		"use_cpu":   c.Config.PromptGuard.UseCPU,
 	})
 
-	return c.jailbreakInitializer.Init(c.Config.PromptGuard.ModelID, c.Config.PromptGuard.UseCPU, numClasses)
+	if err := c.jailbreakInitializer.Init(c.Config.PromptGuard.ModelID, c.Config.PromptGuard.UseCPU, numClasses); err != nil {
+		return err
+	}
+	c.jailbreakModelReady = true
+	return nil
 }
 
 // CheckForJailbreak analyzes the given text for jailbreak attempts.
@@ -202,5 +207,9 @@ func (c *Classifier) initializePIIClassifier() error {
 		"use_cpu":   c.Config.PIIModel.UseCPU,
 	})
 
-	return c.piiInitializer.Init(c.Config.PIIModel.ModelID, c.Config.PIIModel.UseCPU, numPIIClasses)
+	if err := c.piiInitializer.Init(c.Config.PIIModel.ModelID, c.Config.PIIModel.UseCPU, numPIIClasses); err != nil {
+		return err
+	}
+	c.piiModelReady = true
+	return nil
 }
