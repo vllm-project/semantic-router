1a5e60983d768f2d7a031637316321aa1e285a54
JiaoliangYu <jiaolyu@amd.com>
2026-09-04 22:30:55 +0800

    refactor(looper): isolate remom call metadata
    Signed-off-by: JiaoliangYu <jiaolyu@amd.com>

--- src/semantic-router/pkg/looper/remom.go
+++ src/semantic-router/pkg/looper/remom.go
@@ -116,15 +116,12 @@ func (l *ReMoMLooper) remomRunOneParallelCall(
 	}
 
 	startTime := time.Now()
-	resp, err := l.callModelWithContextGate(
+	resp, err := l.dispatchModel(
 		ctx,
 		req,
 		msgCopy,
-		modelName,
-		false,
-		idx+1,
-		nil,
-		accessKeyForModel(req, modelName),
+		ModelTarget{Name: modelName, AccessKey: accessKeyForModel(req, modelName)},
+		CallOptions{DecisionName: req.DecisionName, Iteration: uint32(idx + 1)},
 	)
 	elapsed := time.Since(startTime)
 
@@ -302,8 +299,6 @@ type remomRoundExecution struct {
 
 // Execute implements the Looper interface for ReMoM
 func (l *ReMoMLooper) Execute(ctx context.Context, req *Request) (*Response, error) {
-	l.client.SetDecisionName(req.DecisionName)
-
 	var cfg *config.ReMoMAlgorithmConfig
 	if req.Algorithm != nil && req.Algorithm.ReMoM != nil {
 		cfg = req.Algorithm.ReMoM
