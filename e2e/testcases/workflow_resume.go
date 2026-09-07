5b395d02f2f2f048d1e26b9163dfb65b74438184
pranavthakur0-0 <pranavthakurdew@gmail.com>
2026-09-07 18:58:20 +0530

    fix(e2e): format workflow resume imports
    Signed-off-by: pranavthakur0-0 <pranavthakurdew@gmail.com>

--- e2e/testcases/workflow_resume.go
+++ e2e/testcases/workflow_resume.go
@@ -8,9 +8,10 @@ import (
 	"strings"
 	"time"
 
+	"k8s.io/client-go/kubernetes"
+
 	"github.com/vllm-project/semantic-router/e2e/pkg/fixtures"
 	pkgtestcases "github.com/vllm-project/semantic-router/e2e/pkg/testcases"
-	"k8s.io/client-go/kubernetes"
 )
 
 const workflowRedisKeyPrefix = "vllm-sr:flow:state:"
